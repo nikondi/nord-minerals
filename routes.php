f4e38f7a55730afeb02c517a6a26b60e269c5a7c
nikita <nikondi@narod.ru>
2025-06-19 18:04:23 +0300

    [about] First block

--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Controllers\AboutController;
 use App\Controllers\WelcomeController;
 use App\Router;
 
 Router::get('/', WelcomeController::class);
+Router::get('/about', AboutController::class);
